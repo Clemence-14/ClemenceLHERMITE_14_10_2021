9fdec3a973eecb2eb7d0975adc96b009b311a17c
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2021-10-27 16:42:31 +0200

    Router, post, get, put, delete

---
+++ routes/sauce.js
@@ -0,0 +1,20 @@
+const express = require('express');
+//Création d'un router avec la méthode router d'express
+const router = express.Router();
+
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.post('/', auth, multer, sauceCtrl.createSauce);
+//router.get('/:id', auth, sauceCtrl.getOneSauce);
+//router.get('/', auth, sauceCtrl.getAllSauces);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
+//Pour le like
+//router.post('/:id/like', auth, sauceCtrl.likeDislikeSauce);
+
+module.exports = router;
