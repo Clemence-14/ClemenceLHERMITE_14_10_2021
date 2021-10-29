1c7dc8aa4c66b1f0f4d0f86bc6b684ccf479febb
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2021-10-29 15:30:29 +0200

    signup, login

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,14 @@
 const express = require('express');
+
+//La fonction router
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
+//La route (endpoint) signup
 router.post('/signup', userCtrl.signup);
+//La route login
 router.post('/login', userCtrl.login);
 
+//Exportation du module
 module.exports = router;
