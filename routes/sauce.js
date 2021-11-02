3d266795d01a4ea507b7e71db4d87cc1e5c14c25
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2021-11-02 13:01:56 +0100

    Router, post, get, put, delete

--- routes/sauce.js
+++ routes/sauce.js
@@ -9,8 +9,8 @@ const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
-//router.get('/:id', auth, sauceCtrl.getOneSauce);
-//router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 //router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
