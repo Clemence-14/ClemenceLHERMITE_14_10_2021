094c5ac8b775a7b4fc8431ff3212c4e6b1251f01
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2021-11-03 16:22:46 +0100

    TOKEN

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,9 +4,10 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         //On extraie le token du header authorization de la requête entrante, on utilise split pour tout récupérer après l'espace dans le header
-        const token = req.headers.authorization.split('')[1];
+        const token = req.headers.authorization.split(' ')[1];
+        console.log('token' + token)
         //Pour décoder le token
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         //On extraie l'id de l'utilisateur du token
         const userId = decodedToken.userId;
         if (req.body.userId && req.body.userId !== userId) {
