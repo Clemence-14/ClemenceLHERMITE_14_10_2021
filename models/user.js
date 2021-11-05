7b948355be353966472f7cd36f21f4cc0aa12b15
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2021-11-05 13:46:06 +0100

    Unique validator

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 //Permet de vérifier qu'une adresse mail ne soit pas utilisée deux fois
 const uniqueValidator = require('mongoose-unique-validator');
 
+// On crée un schéma de données qui contient les champs souhaités pour chaque user
 const userSchema = mongoose.Schema({
     email : {type : String, required : true, unique : true },
     password : {type : String, required : true }
@@ -11,4 +12,6 @@ const userSchema = mongoose.Schema({
 //Ajoute le vérificateur d'email unique au schéma
 userSchema.plugin(uniqueValidator);
 
+// On exporte le schéma en tant que modèle mongoose appelé User
+// Permet d'appliquer notre structure de données et aussi de simplifier les opérations de lecture et d'écriture dans la bdd
 module.exports = mongoose.model('User', userSchema);
