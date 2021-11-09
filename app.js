24cec9ac6023c7fa1d566c994e6f2c6f68f68d5a
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2021-11-09 16:10:49 +0100

    MongoDB, CORS

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const path = require('path');
+const path = require('path'); // Indique comment traiter les requêtes
 
 const sauceRoute = require('./routes/sauce');
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/user'); // Importation du router
 
 const app = express();
 
@@ -21,6 +21,9 @@ mongoose.connect(`mongodb+srv://${process.env.APP_USERNAME}:${process.env.APP_PA
 
   
 //CORS
+// Permet de d'accéder à notre api depuis n'importe quelle origine (*)
+// Permet d'ajouter les headers mentionnés aux requêtes envoyées vers l'api (Origin, X-Requested-With...)
+// Permet d'envoyer des requêtes avec les méthodes mentionnées (get, post...)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -32,7 +35,7 @@ app.use((req, res, next) => {
 //app.use(bodyParser.json());  //deprecated
 app.use(express.json());    //Remplace bodyParser
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/auth', userRoutes);
+app.use('/api/auth', userRoutes); // On enregistre le routeur pour toutes les demandes effectuées vers api/auth
 app.use('/api/sauces', sauceRoute);
 
 
